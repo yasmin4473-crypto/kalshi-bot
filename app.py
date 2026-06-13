13b223fd4c526edb737aa0414d1e91ac04a4018b
Claudio Guedez <claudioguedez12@gmail.com>
2026-06-13 01:36:59 -0400

    Fix Railway port binding

--- app.py
+++ app.py
@@ -363,5 +363,5 @@ def dashboard():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "5000"))
-    app.run(host="127.0.0.1", port=port, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
